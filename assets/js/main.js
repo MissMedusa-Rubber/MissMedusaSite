c4541abd7c5da9187366735ff5dedf7b687398de
saydoer <charlieden@gmail.com>
2025-12-13 16:02:13 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
-  
-  /**
-   * SurveyJS
-   */
-  const surveyJson = {
-    elements: [{
-        name: "FirstName",
-        title: "Enter your first name:",
-        type: "text"
-    }, {
-        name: "LastName",
-        title: "Enter your last name:",
-        type: "text"
-    }]
-  };
-  
-  const survey = new Survey.Model(surveyJson);
 
   /**
    * Preloader
